8f156de849f1f4b5f3863f47349da1e692e7bd2b
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2024-11-19 16:44:46 +0000

    fontsize changed done

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import { students } from './data/StudentsDb';
 import StudentTable from './components/StudentTable';
-
+import {useState} from 'react';
 
 function App() {
+  const [fontSize, setFontSize] = useState(16); 
+  const handleFontSize = (size) => {
+    if (size === "S") setFontSize(12);
+    else if (size === "M") setFontSize(16);
+    else if (size === "L") setFontSize(20);
+  };
+
   return (
-    <div className="App">
+    <>
+    <div className="App" style={{fontSize :`${fontSize}px`}}>
       <h1>Students Information Portal</h1>
       <hr/>
+
+      <div className="fontSize" >
+          Font Size :  
+          <button onClick={()=>handleFontSize("S")}>S</button>
+          <button onClick={()=>handleFontSize("M")}>M</button>
+          <button onClick={()=>handleFontSize("L")}>L</button>
+        </div>
       <table width="100%">
         <tbody>
           <tr>
@@ -17,9 +32,12 @@ function App() {
           </tr>
         </tbody>
       </table>
-
     </div>
+    </>
+    
   );
 }
 
 export default App;
+
+
