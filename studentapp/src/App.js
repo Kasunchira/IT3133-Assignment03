69057febf57859ab83e5e598ecbdc414795154c8
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2024-11-19 09:27:43 +0000

    students data is pass to child

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { students } from './data/StudentsDb';
+import StudentTable from './components/StudentTable';
+
 
 function App() {
   return (
@@ -10,7 +13,7 @@ function App() {
         <tbody>
           <tr>
             <td>
-              
+              <StudentTable students={students}/>
             </td>
           </tr>
         </tbody>
